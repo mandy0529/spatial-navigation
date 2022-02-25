7d2d80120d05464ca9cfa748c1fd4136e2711b18
mandy0529 <kmt000529@gmail.com>
2022-02-25 16:53:14 +0900

    practice spatial-navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  withFocusable,
+  initNavigation,
+} from "@noriginmedia/react-spatial-navigation";
+import { useEffect, useState } from "react";
+
+initNavigation({
+  debug: true,
+});
+
+const styles = {
+  focused: {
+    background: "teal",
+  },
+};
+
+const Cell = ({ value, focused }) => {
+  return (
+    <div className="item" style={focused ? styles.focused : {}}>
+      {value}
+    </div>
+  );
+};
+
+const Container = ({ children, setFocus }) => {
+  useEffect(() => {
+    setFocus();
+  }, [setFocus]);
+
+  setTimeout(() => {
+    setFocus();
+  }, 2000);
+
+  return <div className="container">{children}</div>;
+};
+
+const FocusableCell = withFocusable()(Cell);
+const FocusableContainer = withFocusable()(Container);
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
+  const [added, setAdded] = useState(false);
+
+  setTimeout(() => {
+    setLoaded(true);
+  }, 1000);
+
+  const handleClick = () => {
+    setAdded(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <button onClick={handleClick}>Add cells</button>
+        <FocusableContainer>
+          {loaded && (
+            <>
+              <div className="row">
+                <FocusableCell focusKey="1" value="1" />
+                <FocusableCell value="2" />
+                <FocusableCell value="3" />
+              </div>
+              <div className="row">
+                <FocusableCell value="4" />
+                <FocusableCell value="5" />
+                <FocusableCell value="6" />
+              </div>
+              <div className="row">
+                <FocusableCell value="7" />
+                <FocusableCell value="8" />
+                <FocusableCell value="9" />
+              </div>
+              {added && (
+                <>
+                  <div className="row">
+                    <FocusableCell value="10" />
+                    <FocusableCell value="11" />
+                    <FocusableCell value="12" />
+                  </div>
+                </>
+              )}
+            </>
+          )}
+        </FocusableContainer>
+      </>
     </div>
   );
 }
